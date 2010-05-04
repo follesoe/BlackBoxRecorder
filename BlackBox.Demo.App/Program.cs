5a44605a070a8b2f49dc5a733ea80bfe13eb8d86
Jonas Folles <jonas@follesoe.no>
2010-05-04 20:11:05 +0200

    Added new demo projects.

---
+++ BlackBox.Demo.App/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlackBox.Demo.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("BlackBox Recorder Demo Application");
+            Console.ReadLine();
+        }
+    }
+}
