9d136bd6a44f988b9460a6ec527919e4efc16782
Marcus Almgren <MarcusAlmgren@gmail.com>
2010-05-11 22:02:52 +0200

    Uhm, trying to commit the newly added files here

---
+++ BlackBox/Testing/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Test.ObjectComparison;
+
+namespace BlackBox.Testing
+{
+    public static class Extensions
+    {
+        public static string ToMismatchDetailsString(this IEnumerable<ObjectComparisonMismatch> mismatches)
+        {
+            var toStringBuilder = new StringBuilder();
+            if(mismatches.Any())
+            {
+                toStringBuilder.AppendLine("Mismatches:" + Environment.NewLine);
+                mismatches.ToList().ForEach(m => toStringBuilder.AppendLine(m.ToString()));
+            }
+            return toStringBuilder.ToString();
+        }
+    }
+}
