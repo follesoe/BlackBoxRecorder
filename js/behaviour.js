f6995bb0b8be661124f78de524cb36faaec1e80e
Hege Rokenes <hege@rokenes.com>
2010-05-18 22:56:40 +0200

    Slidefunction in place

--- js/behaviour.js
+++ js/behaviour.js
@@ -3,19 +3,43 @@
 });
 
 
-//$(document).ready(function () {
-//    $(".tweet").hover(
-//        function () {
-//            $(".twitter_div", this).addClass("viewLast");
-//        },
-//        function () {
-//            $(".twitter_div", this).removeClass("viewLast");
-//        }
-//    );
-//});
-
 $(document).ready(function () {
-    $('#mycarousel').animate({
-             
+    var currentPosition = 0;
+    var slideWidth = 450;
+    var slides = $("#slideShow li");
+    var numberOfSlides = slides.length;
+
+    console.log(numberOfSlides);
+
+    $(".content").css("overflow", "hidden");
+
+    $("ul#slideShow").css("width", slideWidth * numberOfSlides);
+
+    manageControls(currentPosition);
+
+    $(".slideBtn").bind("click", function () {
+        currentPosition = ($(this).attr("id") == "next") ? currentPosition + 1 : currentPosition - 1;
+        manageControls(currentPosition);
+
+        $("#slideShow").animate({
+            "left": slideWidth * (-currentPosition)
+        });
     });
+
+    function manageControls(position) {
+        if (position == 0) {
+            $("#prev").hide();
+        }
+        else {
+            $("#prev").show();
+        }
+
+        if (position == numberOfSlides - 1) {
+            $("#next").hide();
+        }
+        else {
+            $("#next").show();
+        }
+    }
 });
+
